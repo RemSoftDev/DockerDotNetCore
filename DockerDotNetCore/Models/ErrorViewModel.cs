b5e5a837ddc39ef554eb4763226284ab067260ea
Oleksandr Dubyna <struggleendlessly@hotmail.com>
2018-10-19 20:27:52 +0300

    u

---
+++ DockerDotNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DockerDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
